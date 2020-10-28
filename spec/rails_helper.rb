bc663882445ef75939b49b1c3cee4ca4d342aa29
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-10-28 19:27:13 +0300

    authorize Api requests and respective tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,8 +49,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  config.include RequestSpecHelper, type: :request
-
+  # config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
