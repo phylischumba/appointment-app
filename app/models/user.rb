b9659b625df323b903e079647100164b11b866a3
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-10-28 19:03:26 +0300

    add JWT gem and configure

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-#   has_secure_password
+has_secure_password
 end
