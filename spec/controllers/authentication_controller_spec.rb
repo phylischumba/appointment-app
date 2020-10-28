5a90919e928cfabf0f93bb9d3fb4ce58dc5a3108
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-10-28 21:14:50 +0300

    create authentication controller and application controller tests

---
+++ spec/controllers/authentication_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticationController, type: :controller do
+
+  # describe "GET #g" do
+  #   it "returns http success" do
+  #     get :g
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe "GET #controller" do
+    it "returns http success" do
+      get :controller
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #Authentication" do
+    it "returns http success" do
+      get :Authentication
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
