9bab185e138a889baefa79682721e1ff5dbb1006
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-10-28 20:53:42 +0300

    Update authentication controller and dd tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'authenticationrails/g'
+  get 'authenticationrails/controller'
+  get 'authenticationrails/Authentication'
   resources  :users, :doctors
 
   post 'signup', to: 'users#create'
